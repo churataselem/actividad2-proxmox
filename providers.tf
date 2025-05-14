d5c54605a13bd33ff58ccf85a1a64dd541dac423
Selem Cedec Luna Churata~ <selemcedec@gmail.com>
2025-05-14 14:11:10 -0400

    Subiendo configuración de Terraform para Proxmox

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_tls_insecure      = true
+  pm_api_url           = "https://200.9.165.138:8006/api2/json"
+  pm_api_token_id      = "terra@pve!te"
+  pm_api_token_secret  = "69622"
+}
